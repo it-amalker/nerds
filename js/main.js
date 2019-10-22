06b3520e1b66c08614c71f60d580d602c3f4afce
amalker <it-amalker@yandex.ru>
2019-10-22 11:13:33 +0300

    change slider script (due to IE11 support), delete unused HTML markup

--- js/main.js
+++ js/main.js
@@ -4,25 +4,25 @@ var modalOpenBtn = document.querySelector(".modal-button");
 var modalCloseBtn = document.querySelector(".close-modal-button");
 
 // modal popup open
-modalOpenBtn.addEventListener("click", function(evt) {
+modalOpenBtn.addEventListener("click", function (evt) {
     evt.preventDefault();
-    modal.classList.add("modal--show"); 
+    modal.classList.add("modal--show");
 })
 
 // modal popup close
-modalCloseBtn.addEventListener("click", function(evt) {
-    evt.preventDefault(); 
+modalCloseBtn.addEventListener("click", function (evt) {
+    evt.preventDefault();
     modal.classList.remove("modal--show");
-      
-    
+
+
 })
 
 /* slider switches */
 var switches = document.querySelectorAll(".slider-circle");
 var sliders = document.querySelectorAll(".slider");
 
-switches.forEach(function(elem) {
-    elem.addEventListener("click", function() {
+var changeSlider = function (elem) {
+    elem.addEventListener("click", function () {
         for (var i = 0; i < switches.length; i++) {
             switches[i].classList.remove("active");
             sliders[i].classList.remove("active");
@@ -30,11 +30,13 @@ switches.forEach(function(elem) {
         var switchesArray = Array.prototype.slice.call(switches);
         var sliderIndex = switchesArray.indexOf(elem);
         elem.classList.add("active");
-        sliders[sliderIndex].classList.add("active");   
+        sliders[sliderIndex].classList.add("active");
     });
-});
+}
 
-
+for (var i = 0; i < switches.length; i++) {
+    changeSlider(switches[i]);
+}
 
 
 
