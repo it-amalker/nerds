e69f5e63fdd4e21fba86985268826e5ee0e97790
amalker <it-amalker@yandex.ru>
2019-10-29 10:56:44 +0300

    change script for sliders, add some code to modal script

--- js/main.js
+++ js/main.js
@@ -1,42 +1,47 @@
-/* popup close/open script */
-var modal = document.querySelector(".modal");
-var modalOpenBtn = document.querySelector(".modal-button");
-var modalCloseBtn = document.querySelector(".close-modal-button");
+let modal = document.querySelector(".modal");
+let modalOpenBtn = document.querySelector(".modal-button");
+let modalCloseBtn = document.querySelector(".close-modal-button");
 
-// modal popup open
-modalOpenBtn.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    modal.classList.add("modal--show");
-})
-
-// modal popup close
-modalCloseBtn.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    modal.classList.remove("modal--show");
+let allSwitches = document.querySelectorAll(".slider-circle");
+let allSliders = document.querySelectorAll(".slider");
 
 
-})
+// modal popup open/close
+
+if (modal) {
+    modalOpenBtn.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        modal.classList.add("modal--show");
+    })
+    modalCloseBtn.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        modal.classList.remove("modal--show");
+    })
+}
 
 /* slider switches */
-var switches = document.querySelectorAll(".slider-circle");
-var sliders = document.querySelectorAll(".slider");
 
-var changeSlider = function (elem) {
-    elem.addEventListener("click", function () {
-        for (var i = 0; i < switches.length; i++) {
-            switches[i].classList.remove("active");
-            sliders[i].classList.remove("active");
+let toggleSliders = function (allSwitches, switchElement, allSliders) {
+    switchElement.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        for (let i = 0; i < allSwitches.length; i++) {
+            allSwitches[i].classList.remove("active");
+            allSliders[i].classList.remove("active");
         }
-        var switchesArray = Array.prototype.slice.call(switches);
-        var sliderIndex = switchesArray.indexOf(elem);
-        elem.classList.add("active");
-        sliders[sliderIndex].classList.add("active");
+        let switchesArray = Array.prototype.slice.call(allSwitches);
+        let sliderIndex = switchesArray.indexOf(switchElement);
+        switchElement.classList.add("active");
+        allSliders[sliderIndex].classList.add("active");
     });
 }
 
-for (var i = 0; i < switches.length; i++) {
-    changeSlider(switches[i]);
+let launchToggleSliders = function (switchesList, slidersList) {
+    for (let i = 0; i < switchesList.length; i++) {
+        toggleSliders(switchesList, switchesList[i], slidersList);
+    }
 }
+
+launchToggleSliders(allSwitches, allSliders);
 
 
 
